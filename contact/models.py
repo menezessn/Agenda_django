69761b48572177ba6fa901fe7dd7a9e4ca8d0aef
Marcelo de Menezes Nascimento <mar_chelo10@hotmail.com>
2023-11-24 17:14:02 -0300

    configurações iniciais

---
+++ contact/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Contact (models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
+
